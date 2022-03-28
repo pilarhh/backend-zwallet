ba12c6127b187a94e5843f53170280ab9fb16b6e
pilarhh <pilarhengkiriang11@gmail.com>
2022-03-28 15:07:45 +0700

    delete redis

--- src/controllers/wallets.js
+++ src/controllers/wallets.js
@@ -2,7 +2,6 @@
 const modelWallets = require('../models/wallets')
 const createError = require('http-errors')
 const commonHelper = require('../helpers/common')
-const client = require('../config/redis')
 const modelTransaction = require('../models/transaction')
 
 const getWallets = async (req, res, next) => {
@@ -16,7 +15,6 @@ const getWallets = async (req, res, next) => {
     })
     const resultCount = await modelWallets.countWallets()
     const { total } = resultCount[0]
-    client.setEx('product', 60 * 60, JSON.stringify(result))
     commonHelper.response(res, result, 200, 'data found', {
       currentPage: page,
       limit: limit,
