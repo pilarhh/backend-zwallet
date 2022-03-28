5e63eb319e7621f5110a88c95d1e763047bd04e6
pilarhh <pilarhengkiriang11@gmail.com>
2022-03-28 20:53:37 +0700

    change db

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'qwerty123',
-  database: 'zwallet'
+  host: '44.202.197.206',
+  user: 'fazz3pilar',
+  password: 'pilarhFazz1234#',
+  database: 'fazz3pilar_zwallet-br'
 })
 
 module.exports = connection
