b3749696988c537f6c37859f622463231513e907
pilarhh <pilarhengkiriang11@gmail.com>
2022-04-05 01:59:37 +0700

    edit database

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: '44.202.197.206',
+  user: 'fazz3pilar',
+  password: 'pilarhFazz1234#',
+  database: 'fazz3pilar_zwallet-br'
 })
 
 module.exports = connection
